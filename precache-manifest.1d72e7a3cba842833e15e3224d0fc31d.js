9c1736691c45f91b2ddc0e3d381e9f47a17ac2e5
Thomas Museus Dabay <tmuseusdabay@pm.me>
2020-01-07 15:16:03 -0600

    Updates

---
+++ precache-manifest.1d72e7a3cba842833e15e3224d0fc31d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64060b034154795052109e1cdbd235aa",
+    "url": "/learn-polish-calc/index.html"
+  },
+  {
+    "revision": "e7c4d3cbffcd6757a1b5",
+    "url": "/learn-polish-calc/static/css/main.5e677c04.chunk.css"
+  },
+  {
+    "revision": "7c0ec3b2a1b68ccaf18f",
+    "url": "/learn-polish-calc/static/js/2.78700181.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learn-polish-calc/static/js/2.78700181.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7c4d3cbffcd6757a1b5",
+    "url": "/learn-polish-calc/static/js/main.89d615df.chunk.js"
+  },
+  {
+    "revision": "29b7d1712c11ae58ebf2",
+    "url": "/learn-polish-calc/static/js/runtime-main.60257347.js"
+  },
+  {
+    "revision": "52f9b35f9f7cfa1be2644bcbac61a983",
+    "url": "/learn-polish-calc/static/media/RobotoCondensed-Bold.52f9b35f.ttf"
+  },
+  {
+    "revision": "5b45554305562f2fc404839bb9fe418a",
+    "url": "/learn-polish-calc/static/media/RobotoCondensed-Light.5b455543.ttf"
+  },
+  {
+    "revision": "0e1821fdf320fddc0e1c2b272c422068",
+    "url": "/learn-polish-calc/static/media/RobotoCondensed-Regular.0e1821fd.ttf"
+  },
+  {
+    "revision": "567860c5074f320995d5c58e1b4a3ec7",
+    "url": "/learn-polish-calc/static/media/bitwise.567860c5.ttf"
+  },
+  {
+    "revision": "f6e67539e25adbf860808313c8e75ce5",
+    "url": "/learn-polish-calc/static/media/digital-7 (mono italic).f6e67539.ttf"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/learn-polish-calc/static/media/digital-7 (mono).58045dab.ttf"
+  }
+]);
